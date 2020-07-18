d2fec11eba9abdac40503735db48996728fa9158
sergey@korshunov.pro <korshunov.pro>
2020-07-18 21:11:41 +0300

    check unit exists method, exception if unit is not exists, rename $measure to $timeUnit. init test file

---
+++ tests/TimeIntervalTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Korshunov\TimeInterval\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TimeIntervalTest extends TestCase
+{
+
+    public function testCreateFromHMS()
+    {
+
+    }
+
+    public function testGetSeconds()
+    {
+
+    }
+
+    public function testSub()
+    {
+
+    }
+
+    public function testConvertToMinutes()
+    {
+
+    }
+
+    public function testAdd()
+    {
+
+    }
+
+    public function testConvertToHours()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testGetMinutes()
+    {
+
+    }
+
+    public function testConvertToSeconds()
+    {
+
+    }
+
+    public function testCreateFromDateString()
+    {
+
+    }
+
+    public function testFormat()
+    {
+
+    }
+
+    public function testCreateDateInterval()
+    {
+
+    }
+
+    public function testModify()
+    {
+
+    }
+
+    public function testGetHours()
+    {
+
+    }
+}
